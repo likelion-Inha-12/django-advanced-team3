7720bb0ab83ce613ee2351d9a807a74599f55220
kaswhy <sxshx818@naver.com>
2024-04-07 14:26:51 +0900

    feat: add name

--- team3_project/crudmember/models.py
+++ team3_project/crudmember/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     personal_key = models.IntegerField() # id(pk)
+    name = models.CharField(max_length=10) # 사람 이름, 최대 길이 10글자
     email = models.CharField(max_length=100) # 100글자가 최대인 문자열
     is_leader = models.BooleanField() # 대표 여부
     hearts = models.IntegerField() # 하트 수
