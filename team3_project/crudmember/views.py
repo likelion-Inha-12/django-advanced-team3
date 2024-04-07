4cae39c41b24515bd74de85c8b504340ec5288bf
kaswhy <sxshx818@naver.com>
2024-04-07 15:41:50 +0900

    fix : update argument pk

--- team3_project/crudmember/views.py
+++ team3_project/crudmember/views.py
@@ -28,8 +28,6 @@ crudmember/models.py
 def addUser(request):
     if request.method == 'POST':
         data = json.loads(request.body)
-
-        name = data.get('name')
         email = data.get('email')
         is_leader = data.get('is_leader')
         hearts = data.get('hearts')
@@ -37,7 +35,6 @@ def addUser(request):
         user_name = data.get('user_name')
 
         post = Post(
-            name = name,
             email = email,
             is_leader = is_leader,
             hearts = hearts,
@@ -50,9 +47,9 @@ def addUser(request):
     return JsonResponse({'message' : 'POST 요청만 허용됩니다.'})
 
 
-def userInfo(request, personal_key): # 입력값: id (personal_key) -> 출력값: id, email, 대표 여부, 하트 수
+def userInfo(request, pk): # 입력값: id (personal_key) -> 출력값: id, email, 대표 여부, 하트 수
     if request.method == 'GET':
-        post = get_object_or_404(Post, personal_key = personal_key)
+        post = get_object_or_404(Post, pk = pk)
         data = {
             'id' : post.personal_key,
             'email' : post.email,
